53837795fec8e7c4e791cfc62080c4a49528f21d
Руслан Абдулазимов <abdulazimov.03@mail.ru>
2022-01-30 13:25:19 +0500

    3

--- js/main.js
+++ js/main.js
@@ -3,19 +3,27 @@ import { UI_COMPONENTS } from "./view.js";
 
 const variants = ['years', 'days', 'hours'];
 const nowDate = moment(new Date())
+let indexForVariants = 0
 
 UI_COMPONENTS.DATE_FORM.addEventListener('submit', event => {
   const inputValue = UI_COMPONENTS.DATE_INPUT.value
   const userDate = moment(new Date(UI_COMPONENTS.DATE_INPUT.value))
-  
+
   if (inputValue) {
-    console.log(inputValue);
-    for (const variant of variants) {
-      UI_COMPONENTS.DATE_RESULT.innerHTML += `<div>${variant}: <span>${userDate.diff(nowDate, variant)}</span>.</div>`
-    }
+    UI_COMPONENTS.DATE_RESULT.innerHTML = ''
+
+    getTime(userDate)
   } else {
     alert('Введите дату.')
   }
-  
+
   event.preventDefault()
 })
+
+function getTime(userDate) {
+  UI_COMPONENTS.DATE_RESULT.innerHTML += `<div>${variants[indexForVariants]}: <span>${userDate.diff(nowDate, variants[indexForVariants])}</span>.</div>`
+  indexForVariants++
+  if(indexForVariants < variants.length) {
+    getTime(userDate)
+  }
+}
